561a8543e67e7914afdb2dc5dd5d6fbea30362db
Mustek <imustek@gmail.com>
2016-04-04 21:11:43 +0200

    New module: Add security level where missing

---
+++ checks/ModuleSecurity.js
@@ -0,0 +1,35 @@
+/**
+ * This module fixes security levels if they are missing
+ *
+ * This file is part of ArisaJS
+ */
+
+"use strict";
+
+var Module = require('../lib/module.class.js');
+var log = require('../util/logger');
+
+
+module.exports = class ModuleSecurity extends Module {
+    constructor({config}) {
+        super({config});
+    }
+
+    check({issue}) {
+
+        return new Promise(function (resolve, reject) {
+            let fields = issue.fields;
+
+
+            if(fields.security === undefined && issue.project.security !== null){
+
+                issue.addSecurityLevel(issue.project.security.public);
+                resolve(null);
+
+            }else resolve(null);
+
+
+
+        });
+    }
+};
