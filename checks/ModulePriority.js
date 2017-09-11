75b2e8e3734fc8cd90349e972f56309c6739a230
Bemoty <signup.bemoty@outlook.com>
2017-09-11 21:31:18 +0200

    Adding ModulePriority.js

---
+++ checks/ModulePriority.js
@@ -0,0 +1,22 @@
+"use strict";
+
+var Module = require('../lib/module.class.js');
+
+module.exports = class ModulePriority extends Module {
+    constructor({config}) {
+        super({config});
+    }
+
+    check({issue}) {
+
+        return new Promise(function (resolve, reject) {
+            let priority = issue.fields.priority.name;
+
+            if(priority == null && issue.key == "WEB") {
+                issue.setPriority();
+            }
+            
+            resolve(null);
+        });
+    }
+};
